ca903ec16d1c0599fd5237f19a781d10d9152ea6
adiya-deokar <adityadeokar80@gmail.com>
2025-05-15 19:49:52 +0530

    feat: implement CreatePage and RenderPage components with Zustand for state management

--- src/app/(protected)/(pages)/(dashboardPages)/create-page/page.tsx
+++ src/app/(protected)/(pages)/(dashboardPages)/create-page/page.tsx
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react'
 import { redirect } from 'next/navigation'
 import CreatePageSkeleton from './_components/CreatePageSkeleton'
 import { onAuthenticateUser } from '@/actions/user'
+import RenderPage from './_components/RenderPage'
 
 const page = async() => {
  const checkUser = await onAuthenticateUser()
@@ -10,14 +11,14 @@ const page = async() => {
      redirect('/sign-in')
    }
  
-   if(!checkUser.user.subscription){
-      redirect('/dashboard')
-   }
+  //  if(!checkUser.user.subscription){
+  //     redirect('/dashboard')
+  //  }
 
   return (
     <main className="w-full h-full pt-6">
       <Suspense fallback={<CreatePageSkeleton />}>
-       
+        <RenderPage />
       </Suspense>
     </main>
   )
